53082f1971b92ab47e1e517993f83273789d048b
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2023-02-07 23:51:07 +0530

    incorporated hardware sensor

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -17,17 +17,52 @@ import {
   View,
 } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  accelerometer,
+  gyroscope,
+  setUpdateIntervalForType,
+  SensorTypes
+} from "react-native-sensors";
+import { map, filter } from "rxjs/operators";
 
+setUpdateIntervalForType(SensorTypes.accelerometer, 400);
 
+const subscription = accelerometer
+  .pipe(map(({ x, y, z }) => x + y + z), filter(speed => speed > 20))
+  .subscribe(
+    speed => console.log(`You moved your phone with ${speed}`),
+    error => {
+      console.log("The sensor is not available");
+    }
+  );
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({navigation}) {
+  const [visible,setVisible]=useState(true)
+  setTimeout(()=>{
+    setVisible(false)
+  },4000)
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>LOGO HERE {visible ? "logo": "logo"} </Text>
+    </View>
+  );
+}
 
 
 function App(): JSX.Element {
 
 
   return (
-    <SafeAreaView >
-
-    </SafeAreaView>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
